3b7bba2bb7947872a0823a39013d6962152ab298
James Inman <james@jamesinman.co.uk>
2024-09-24 00:26:34 +0100

    Add Rails engine `bin/rails` back

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,6 @@
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "/panda-cms-assets/javascripts/vendor/stimulus-loading.js" # 3.2.2
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.2.0/app/assets/javascripts/activestorage.esm.js"
 
 pin "panda_cms/vendor/stimulus-components-rails-nested-form", to: "/panda-cms-assets/javascripts/vendor/stimulus-components-rails-nested-form.js", preload: false
