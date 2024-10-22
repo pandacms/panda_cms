62f3e1df8c7e088c4438d6f7131d65c92fcfd9f3
James Inman <james@jamesinman.co.uk>
2024-10-23 00:43:23 +0100

    Fix 404 tests

--- spec/system/admin/pages/edit_page_spec.rb
+++ spec/system/admin/pages/edit_page_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe "Edit a page", js: true do
   scenario "when the user is not logged in it returns a 404 error" do
     homepage = create(:homepage, title: "Home Page")
     visit "/admin/pages/#{homepage.id}/edit"
-    expect(page).to have_content("The page you were looking for could not be found. Sad panda.")
+    expect(page).to have_content("The page you were looking for doesn't exist.")
   end
 
   scenario "when the user is logged in as a user (not admin) it returns a 404 error", js: true do
     homepage = create(:homepage, title: "Home Page")
     create_and_login_user_with(:github)
     visit "/admin/pages/#{homepage.id}/edit"
-    expect(page).to have_content("The page you were looking for could not be found. Sad panda.")
+    expect(page).to have_content("The page you were looking for doesn't exist.")
   end
   # todo: permissions here?
 
