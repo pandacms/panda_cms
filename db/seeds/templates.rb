d7bc48bf535987dc9ebb462c6eb870eb8fd957c4
James Inman <james@jamesinman.co.uk>
2024-10-26 21:33:55 +0100

    Remove random variable assignment

--- db/seeds/templates.rb
+++
@@ -1,3 +0,0 @@
-homepage = templates.create :homepage, name: "Homepage", file_path: "layouts/homepage", max_uses: 1
-page = templates.create :page, name: "Page", file_path: "layouts/page"
-embed = templates.create :embed, name: "Embed", file_path: "layouts/embed"
